fe4cb7521f29a03b24fd07269049df7b2daf2150
Lenin <lerama@floridauniversitaria.es>
2022-03-11 13:30:35 +0100

    Plantilla e imagenes para futuro uso.

---
+++ plantilla.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="img/logo_icon.ico">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,700;0,900;1,400;1,700;1,900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css">
+    <link rel="stylesheet" href="css/general-style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css%22%3E">
+    <title>Helios Corp.</title>
+</head>
+
+<body class="card">
+    <!-- HEADER -->
+    <header class="card">
+        <div class="container logo-nav-container">
+            <a href="index.php">
+                <!-- LOGO -->
+                <img class="main_logo" src="img/logo5.png" alt="main_logo">
+            </a>
+            <!-- BARRA DE NAVEGACIÓN -->
+            <!-- Falta añadir los enlaces en el href y los alt -->
+            <div class="navbar">
+                <div class="navbar-end">
+                    <div class="navbar-item">
+                        <a href="#">
+                            <div class="icon-text navbar-item">
+                                <span class="icon is-large">
+                                    <img src="img/usuario.png" alt="">
+                                </span>
+                                <span>Usuario</span>
+                            </div>
+                        </a>
+                        <a href="#">
+                            <div class="icon-text navbar-item">
+                                <span class="icon is-large">
+                                    <img src="img/mensajes.png" alt="">
+                                </span>
+                                <span>Mensajes</span>
+                            </div>
+                        </a>
+                        <a href="#">
+                            <div class="icon-text navbar-item">
+                                <span class="icon is-large">
+                                    <img src="img/cerrar.png" alt="">
+                                </span>
+                                <span>Desconectar</span>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- FIN DEL HEADER -->
+
+    <!-- MAIN SECTION -->
+    <main>
+        <!-- Block para dar espacio con el header, uno vacío primero para que haya un espacio con el segundo -->
+        <div class="block"></div>
+
+
+        <!-- AQUÍ EMPIEZA EL CONTENIDO DE LA PÁGINA -->
+        <!-- DENTRO DEL SIGUIENTE DIV.BLOCK VA EL CONTENIDO DE LA PÁGINA-->
+        <div class="block">
+            <!-- ¡¡¡¡CONTENIDO AQUÍ!!!! -->
+        </div>
+        <!-- FIN DEL CONTENIDO DE LA PÁGINA -->
+
+
+        <!-- Otro bloque para dar espacio con el Footer -->
+        <div class="block"></div>
+    </main>
+    <!-- FIN DE LA MAIN SECTION -->
+
+    <!-- FOOTER -->
+    <footer class="card">
+        <div class="container logo-nav-container">
+            <a href="#">
+                <div class="icon-text navbar-item">
+                    <span class="icon is-large">
+                        <img src="img/flecha-hacia-atras.png" alt="">
+                    </span>
+                </div>
+            </a>
+        </div>
+    </footer>
+    <!-- FIN DEL FOOTER -->
+</body>
+
+</html>
