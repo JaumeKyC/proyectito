4303ce4689c0abfd0b4f27cab35502a897d72eea
Lenin <94408917+NinxCode@users.noreply.github.com>
2022-05-20 22:28:58 +0200

    Update scriptPedidos.js

--- js/scriptPedidos.js
+++ js/scriptPedidos.js
@@ -78,12 +78,8 @@ function drawPedidoInfo(data) {
     for (let i = 0; i < data.length; i++) {
         let row = document.createElement("tr");
         for (field in data[i]) {
-/*             let th = document.createElement("th");
-            th.innerHTML = field.charAt(0).toUpperCase() + field.slice(1); */
             let td = document.createElement("td");
             td.innerHTML = data[i][field];
-
-            /* row.appendChild(th); */
             row.appendChild(td);
             clientData.appendChild(row);
         } 
